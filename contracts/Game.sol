576813b02b4464706826bd113ef45ea4635da6ae
Daniel Nolan <DanNolan99@gmail.com>
2020-11-02 10:54:27 -0500

    simpler interface

--- contracts/Game.sol
+++ contracts/Game.sol
@@ -2,12 +2,9 @@
 pragma solidity ^0.7.3;
 
 contract Game {
-  constructor() public payable {}
-
   event Winner(address winner);
 
   function win() public {
     emit Winner(msg.sender);
-    msg.sender.transfer(address(this).balance);
   }
 }
