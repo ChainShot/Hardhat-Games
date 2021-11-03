0ff57afa1d02ef78949d7294550b8ccd8df5c181
AlvaroLuken <al5031@nyu.edu>
2021-11-03 09:33:43 -0700

    Updates to activity!

--- contracts/IGame.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: Unlicense
-pragma solidity ^0.7.3;
-
-interface IGame {
-  event Winner(address winner);
-
-  function win() external;
-}
--- scripts/win.js
+++ scripts/win.js
@@ -1,7 +1,7 @@
 const GAME_ADDRESS = "";
 
 async function main() {
-  const game = await hre.ethers.getContractAt("IGame", GAME_ADDRESS);
+  const game = await hre.ethers.getContractAt("Game1", GAME_ADDRESS);
 
   const tx = await game.win();
   const receipt = await tx.wait();
@@ -9,8 +9,6 @@ async function main() {
   console.log(receipt);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch(error => {
